a841ebc98e1b0c836c372fb8954071899698d34f
mark-kibo <kibochamark@gmail.com>
2023-10-21 03:03:11 -0700

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response
-from flask_migrate import Migrate
-
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
@@ -10,7 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 db.init_app(app)
 
 @app.route('/')
@@ -19,16 +20,66 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal=Animal.query.get(id)
+    if animal:
+        return f"""
+    <ul>
+        <li>ID: {animal.id}</li>
+        <li>Name: {animal.name}</li>
+        <li>Species: {animal.species}</li>
+        <li>Zookeeper: {animal.zookeeper.name}</li>
+        <li>Enclosure: {animal.enclosure.environment}</li>
+        
+    </ul>
+
+
+"""
+    return None
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper=Zookeeper.query.get(id)
+    animals = ""
+    if zookeeper:
+        for animal in zookeeper.wild:
+            animals += f"<li>Animal: {animal.name}</li>"
+
+        return f"""
+        <ul>
+            <li>ID: {zookeeper.id}</li>
+            <li>Name: {zookeeper.name}</li>
+            <li>Birthday: {zookeeper.birthday}</li>
+            <li>
+                <ul>
+                    {animals}
+                </ul>
+            </li>
+        </ul>
+        """
+    return None
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
+    enclosure=Enclosure.query.get(id)
+    animals=""
+    if enclosure:
+        for animal in enclosure.wild:
+            animals += f"<li>Animal: {animal.name}</li>"
+        return f"""
+        <ul>
+            <li>ID: {enclosure.id}</li>
+            <li>Environment: {enclosure.environment}</li>
+            <li>Open to Visitors: {enclosure.open_to_visitors}</li>
+            <li>
+                <ul>
+                    {animals}
+                </ul>
+            </li>
+        </ul>
+        """
     return ''
 
 
 if __name__ == '__main__':
+    manager.run()
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+import enum
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -7,17 +8,31 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Environment(enum.Enum):
+    GRASS = "grass"
+    SAND = "sand"
+    WATER="water"
+
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    birthday = db.Column(db.String(20))
+    animals = db.relationship("Animal", backref="wild", lazy=True)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
-
+    environment = db.Column(enum.Enum(Environment))
+    open_to_visitors = db.Column(db.Boolean, default=False, nullable=False)
+    animals = db.relationship("Animal", backref="wild", lazy=True)
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    species = db.Column(db.String(20))
+    zookeeper = db.Column(db.Integer(), db.ForeignKey("zookeeper.id"))
+    enclosure = db.Column(db.Integer(), db.ForeignKey("enclosure.id"))
